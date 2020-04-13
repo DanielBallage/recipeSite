0d22fca6a9b2e8252b971a3b2781df86c0032f52
DanielBallage <danieljballage@gmail.com>
2020-04-13 16:37:43 -0400

    pull random meal from api

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const vueTest = new Vue ({
         title: "I hope this works"
     }
 })
+
+new Vue({
+    el: '#randomMeal',
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(response => (this.info = response))
+    }
+  })
