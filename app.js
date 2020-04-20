d65809fc3932baf0ea1657485c523f3f2e49e2ba
DanielBallage <danieljballage@gmail.com>
2020-04-20 19:42:25 -0400

    added vue items function for "all" page

--- app.js
+++ app.js
@@ -14,32 +14,57 @@ new Vue({
         .then(response => (this.info = response.data.meals[0]))   
     }
     
-  })
+  });
 
 
   /* List all Meals in alphabeltical order */
-
-new Vue({
-    el: '#mealsA', 
-    data () {
-      return {
-        info: null,
-      }
-    },
-
+  function meal(a) {
+    new Vue({
+        el: '#meals' + a, 
+        data () {
+          return {
+            info: null,
+          }
+        },
     
-    mounted () {
-      axios
-      
-        .get('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-        .then(response => (this.info = response.data.meals))  
-    }
+        
+        mounted () {
+          axios
+          
+            .get('https://www.themealdb.com/api/json/v1/1/search.php?f=' + a)
+            .then(response => (this.info = response.data))
+        }
+        
+      });
 
-    /* need a for loop to get each item in the array of each letter*/
-    
-  })
 
+}
 
-  
-
+meal("A");
+meal("B");
+meal("C");
+meal("D");
+meal("E");
+meal("F");
+meal("G");
+meal("H");
+meal("I");
+meal("J");
+meal("K");
+meal("L");
+meal("M");
+meal("N");
+meal("O");
+meal("P");
+meal("Q");
+meal("R");
+meal("S");
+meal("T");
+meal("U");
+meal("V");
+meal("W");
+meal("X");
+meal("Y");
+meal("Z");
+ 
   
