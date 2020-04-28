8b5e8b96e9581387c99f59c6982800cc57f1a4b0
DanielBallage <danieljballage@gmail.com>
2020-04-28 17:53:47 -0400

    created test collapse button and a button

--- app.js
+++ app.js
@@ -68,3 +68,17 @@ meal("Y");
 meal("Z");
  
   
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display !== "none") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
